611b265aeac43437f4ebca8c787d0a68c61aae7f
aladin002dz <mahfoudh.arous@gmail.com>
2024-05-19 11:26:15 +0100

    validate locations

--- js/modal.js
+++ js/modal.js
@@ -41,6 +41,8 @@ function validateNames(nameInput) {
   if (nameInput.value.length < 2) {
     const errorMessage = `Veuillez entrer 2 caractères ou plus pour le champ du
      ${nameInput === firstNameInput ? "prénom" : "nom"}`;
+    /* on peut aussi utiliser un message d'erreur générique
+     const errorMessage = "Veuillez entrer 2 caractères ou plus pour ce champ." */
     displayErrorMessages(nameInput, errorMessage);
     return false;
   }
@@ -58,6 +60,69 @@ function validateEmail() {
   return true;
 }
 
+function validateBirthdate() {
+  const birthdateValue = birthdateInput.value;
+  const birthdate = new Date(birthdateValue);
+  const currentDate = new Date();
+  if (birthdateValue === '' || birthdate >= currentDate) {
+    displayErrorMessages(birthdateInput, "Veuillez entrer une date de naissance valide.");
+    return false;
+  }
+  removeErrorMessages(birthdateInput);
+  return true;
+}
+
+function validateQuantity() {
+  const quantity = quantityInput.value;
+  if (quantity === '' || quantity === null || isNaN(quantity)) {
+    displayErrorMessages(quantityInput, "Veuillez entrer un nombre entre 0 et 99.");
+    return false;
+  }
+  removeErrorMessages(quantityInput);
+  return true;
+}
+
+function validateLocation() {
+  const locations = document.querySelectorAll("input[name='location']");
+  for (let i = 0; i < locations.length; i++) {
+    if (locations[i].checked) {
+      removeErrorMessages(locations[0]);
+      return true;
+    }
+  }
+  displayErrorMessages(locations[0], "Veuillez choisir une localisation.");
+  return false;
+}
+
+function validateCheckbox() {
+  const checkbox = document.getElementById("checkbox1");
+  if (!checkbox.checked) {
+    displayErrorMessages(checkbox, "");
+    return false;
+  }
+  removeErrorMessages(checkbox);
+  return true;
+}
+
+
+
+//form submit
+formElement.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  if (!validateNames(firstNameInput) | !validateNames(lastNameInput) | !validateEmail() |
+    !validateBirthdate() | !validateQuantity() | !validateCheckbox() | !validateLocation()) {
+    return;
+  }
+  displayConfirmationMessage();
+}
+
+function displayConfirmationMessage() {
+  formElement.reset();
+  alert("Merci pour votre réservation !");
+}
+
+
 function displayErrorMessages(input, message) {
   const parentElement = input.parentElement;
   parentElement.setAttribute("data-error", message);
@@ -67,21 +132,6 @@ function displayErrorMessages(input, message) {
 function removeErrorMessages(input) {
   const parentElement = input.parentElement;
   parentElement.setAttribute("data-error-visible", false);
-}
-
-//form submit
-formElement.addEventListener("submit", handleSubmit);
-function handleSubmit(event) {
-  event.preventDefault();
-  if (!validateNames(firstNameInput) | !validateNames(lastNameInput) | !validateEmail()) {
-    return;
-  }
-  displayConfirmationMessage();
-}
-
-function displayConfirmationMessage() {
-  formElement.reset();
-  alert("Merci pour votre réservation !");
 }
 
 
